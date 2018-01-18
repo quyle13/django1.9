eb16052b2541649f881b6cd4b4ad62488dc82908
Quy Le <2300821l@ad.dcs.gla.ac.uk>
2018-01-18 20:57:46 +0000

    Template, custom template

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -16,7 +16,9 @@ def index(request):
     context_dict['pages'] = top_page
     return render(request,'rango/index.html',context_dict)
 def about(request):
-    return render(request,'rango/about.html')
+    print (request.method)
+    print (request.user)
+    return render(request,'rango/about.html',{})
 
 def show_category(request,category_name_slug):
     context_dict = {}
